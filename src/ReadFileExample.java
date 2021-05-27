e1c75cdfc3928f4eb256ccfa6ba9d858f8d51565
vuongvantu <vuongvantu2001x@gmail.com>
2021-05-27 14:34:04 +0700

    cmt

---
+++ src/ReadFileExample.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public void readFileText(String filePath){
+        try {
+            File file = new File(filePath);
+            if (!file.exists()){
+                throw new FileNotFoundException();
+            }
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            int sum = 0 ;
+            while ((line = br.readLine())!=null){
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+        } catch (Exception e) {
+            System.err.println("File không tồn tại hoặc có lỗi");
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+
+        ReadFileExample readfileEx = new ReadFileExample();
+        readfileEx.readFileText(path);
+    }
+}
